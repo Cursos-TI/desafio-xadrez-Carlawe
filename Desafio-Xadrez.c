00d239bd655c38789a80049eeda451ae22ddc2a8
Carlawe <freitaswalle05@gmail.com>
2025-07-03 21:21:50 +0000

    ok

--- Desafio-Xadrez.c
+++ Desafio-Xadrez.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
+              //Recursividade
+
+
+              //Movimento da torre
+ void moverTorre(int casas){
+     if(casas > 0){             // Caso base
+     printf ("Direita\n");
+     moverTorre( casas - 1);    
+     }
+   }
+     
+   
+              //Movimento do Bispo
+void moverBispo(int casas){
+      if (casas >  0){              //caso base
+        printf("Cima, direita\n");  
+        moverBispo (casas - 1);     
+     }
+    }
+
+
+             // Movimento da Rainha
+void moverRainha(int casas) {
+    if( casas > 0){             //Caso base
+      printf("Esquerda\n");
+      moverRainha (casas - 1);
+    }
+  }
+
+
 int main(){
 
-int bispo = 0;
-int rainha = 0;
-int cavalo = 0;
+    moverTorre(5);   // Quantidade de vezes que a torre vai se mover
+    printf("\n");    // pular linha
+    moverBispo(5);   // quantidade de vezes que o bispo vai se mover
+    printf("\n");    // pular linha
+    moverRainha(8);  // Quantidade de vezes que a rainha vai se mover
+    printf("\n");    // pular linha
 
-                        //Torre: Andar 5 casas pra frente.
-
-
-for( int torre = 0 ;  torre < 5; torre++){ // enquanto torre for menor que 5 acrescente 1 passo.
-   printf("Direita\n");                    // imprimir direita 5 vezes
-      
-}
-
-   printf("\n");
- 
-
-                       // Bispo: Andar 5 casas na diagonal pra direita.
- while (bispo < 5)  //Enquanto bispo for menor que 5, acrescente 1.
-{
-   printf("Cima direita\n");
-     bispo ++;
-
-}
-   printf("\n");
-
-                            //Rainha: Andar 8 casas pra esquerda.
-do {
-   printf("Esquerda\n"); //Imprima esquerda incrementando 1 enquanto rainha for menor que 8.
-    rainha++;
-
-}while( rainha < 8);
+    int cavalo = 0;         
     
-     printf("\n");
+                          //Movimento do cavalo
 
-                            //Movimento do cavalo
-                       
-while (cavalo < 1){
+  while (cavalo < 1){
       for (int cavalo = 0; cavalo < 2; cavalo++){ //enquanto cavalo for menor que 2, acresente 1 passo.
-            printf("Baixo\n");  //imprima baixo duas vezes
+            printf("Cima\n");  //imprima cima duas vezes
              }
      
-             printf("Esquerda\n"); //imprima esquerda enquanto cavalo for menor que 1.
+             printf("Direita\n"); //imprima direita enquanto cavalo for menor que 1.
              cavalo++;
       
-            }
+            } 
     
-return 0;
+    
+     
+         
+      
 
+                    
+      
+
+ return 0;
 
 }
+
